1b08ee63a20b85e2d4118454f205017e008761f6
Nix <thebyt3zcode@yahoo.com>
2026-07-06 12:16:40 -0300

    Initial commit

---
+++ zip2IMG.cpp
@@ -0,0 +1,20 @@
+﻿// zip2IMG.cpp: define o ponto de entrada para o aplicativo.
+//
+
+#include "zip2IMG.h"
+
+using namespace std;
+
+int main(int argc, char* argv[]) {
+	for (int i = 1; 1 < argc; i++) {
+		std::string arg = argv[i];
+
+		if (arg == "help") {
+			printf("Avaiable arguments: \n");
+		}
+		else if (arg == "-i") {
+			return 0;
+		}
+	}
+	return 0;
+}
